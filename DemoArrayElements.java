7ce708f5c36b7181290d6e9b10dbeaf58e8bf978
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-03 23:05:06 +0300

    Add files via upload

---
+++ DemoArrayElements.java
@@ -0,0 +1,10 @@
+package javatraining.oletsky.arrays;
+
+public class DemoArrayElements {
+    public static void main(String[] args) {
+        int[] mas = {10, 20, 30};
+        System.out.println(mas[1]); //20
+        System.out.println(mas.length);
+        System.out.println(mas[3]); //Exception
+    }
+}
